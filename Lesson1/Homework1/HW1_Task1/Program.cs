91d84f35d6c76d2e216de5a0b371a8b09003593a
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-29 16:39:57 +0300

    Added Tasks 1 and 2 from Homework1 from Lesson1

---
+++ Lesson1/Homework1/HW1_Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW1_Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Lesson1/Homework1/HW1_Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dividend = TryGetNumber("divisible value");
+            var divider = TryGetNumber("divisor");
+            
+            Console.WriteLine(divider == 0 ? "You can't divide by zero!" : $"Result: {dividend / divider}");
+            
+            int TryGetNumber(string message)
+            {
+                Console.Write($"Enter the {message}: ");
+                var value = Console.ReadLine();
+                
+                if (int.TryParse(value, out var number) == false)
+                    throw new FormatException("You entered incorrect data!");
+
+                return number;
+            }
+        }
+    }
+}
