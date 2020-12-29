66270bcf4e74fbd7a523495a23c414ae98235b47
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-29 17:43:01 +0300

    Task3 from Homework2 from Lesson1 is done.

---
+++ Lesson1/Homework2/HW2_Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW2_Task3
+{
+    class Program
+    {
+        enum University
+        {
+            HSE,
+            MSU,
+            MIPT,
+            MEPhI,
+            MTUCI
+        }
+
+        struct Employee
+        {
+            public string Name;
+            public string Surname;
+            public decimal Salary;
+            public University Institue;
+        }
+        
+        static void Main()
+        {
+            Employee person;
+            person.Name = "Yuri";
+            person.Surname = "Fedotov";
+            person.Salary = 31337.77M;
+            person.Institue = University.MTUCI;
+            Console.WriteLine($"Hello, {person.Name} {person.Surname}!");
+            Console.WriteLine($"Your salary is {person.Salary} and you are working in {person.Institue}.");
+        }
+    }
+}
