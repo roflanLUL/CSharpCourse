62203210e11a6fe267ff8a1bbafd3b2e6c69e48f
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-30 01:59:02 +0300

    Added Task5(A and B) from shkolkovo.net

---
+++ Lesson3/Task5/Task5_A/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task5_A
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = new int[n];
+            var maxim = -1;
+
+            for (int i = 0; i < n; ++i)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 2; j < n; ++j)
+                {
+                    if (a[i] * a[j] > maxim)
+                    {
+                        maxim = a[i] * a[j];
+                    }
+                }
+            }
+            Console.WriteLine(maxim);
+        }
+    }
+}
---
+++ Lesson3/Task5/Task5_B/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task5_B
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var line = new int[2];
+
+            var maxim = -1;
+            var maximp = -1;
+
+            for (int i = 0; i < 2; ++i)
+            {
+                line[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 2; i < n; ++i)
+            {
+                var x = int.Parse(Console.ReadLine());
+
+                if (x * maxim > maximp)
+                {
+                    maximp = x * maxim;
+                }
+
+                if (line[0] > maxim)
+                {
+                    maxim = line[0];
+                }
+
+                line[0] = line[1];
+                line[1] = x;
+            }
+            Console.WriteLine(maximp);
+        }
+    }
+}
