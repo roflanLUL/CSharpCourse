ef6c28206902779a59a66bb97f4c917cd02fbd79
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-29 17:31:08 +0300

    Task1 from Homework2 from Lesson1 is done.

---
+++ Lesson1/Homework2/HW2_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HW2_Task1
+{
+    class Program
+    {
+        enum BankAccounts
+        {
+            Current,
+            Credit,
+            Deposit,
+            Budgetary
+        }
+        static void Main()
+        {
+            BankAccounts account = BankAccounts.Credit;
+            Console.WriteLine($"Your bank account type is {account}.");
+        }
+    }
+}
