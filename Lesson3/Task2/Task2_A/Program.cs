6d81a85436f13f948625767048ac0c6aeea05ae9
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-30 01:31:02 +0300

    Added Task2(A and B) from shkolkovo.net

---
+++ Lesson3/Task2/Task2_A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2_A
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = 0;
+            var a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 7; j < n; ++j)
+                {
+                    if (a[i] * a[j] % 3 == 0)
+                    {
+                        k++;
+                    }
+                }
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
---
+++ Lesson3/Task2/Task2_B/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task2_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var line = new int[7];
+            int ans = 0, k3 = 0;
+            for (int i = 0; i < 7; ++i)
+            {
+                line[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 7; i < n; ++i)
+            {
+                var x = int.Parse(Console.ReadLine());
+                
+                if (line[0] % 3 == 0)
+                {
+                    k3++;
+                }
+
+                if (x % 3 == 0)
+                {
+                    ans += i - 8;
+                }
+                else
+                {
+                    ans += k3;
+                }
+
+                for (int j = 0; j < 7; ++j)
+                {
+                    line[j] = line[j + 1];
+                }
+                line[7] = x;
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
