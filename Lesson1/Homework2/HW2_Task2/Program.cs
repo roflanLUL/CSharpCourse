8436c887b9cd44177be188906afe84edfb9573ee
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-29 17:38:19 +0300

    Task2 from Homework2 from Lesson1 is done.

---
+++ Lesson1/Homework2/HW2_Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW2_Task2
+{
+    class Program
+    {
+        enum BankAccounts
+        {
+            Current,
+            Credit,
+            Deposit,
+            Budgetary
+        }
+
+        struct Information
+        {
+            public int number;
+            public BankAccounts account;
+            public decimal balance;
+        }
+        
+        static void Main()
+        {
+            Information info;
+            info.number = 0;
+            info.account = BankAccounts.Budgetary;
+            info.balance = 10005.8M;
+            Console.WriteLine($"Your bank account number is {info.number}, type is {info.account}, balance is {info.balance}");
+        }
+    }
+}
