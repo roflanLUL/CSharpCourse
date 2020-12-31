90fd0653f768b6786eb11378ccc5ba0963920bb4
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 19:25:17 +0300

    Added Task7 from Methods

---
+++ Lesson3/Methods/Methods_Task7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Methods_Task7
+{
+    class Program
+    {
+        static double Add(double x, double y)
+        {
+            var sum = x + y;
+
+            return sum;
+        }
+
+        static double Sub(double x, double y)
+        {
+            var res = x - y;
+
+            return res;
+        }
+        static void Main()
+        {
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+
+            var sum = Add(x, y);
+            var res = Sub(x, y);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(res);
+        }
+    }
+}
