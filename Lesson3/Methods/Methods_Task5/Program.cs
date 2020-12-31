b6550f560e6f446e5d2e8cd53fab3ece8f6a0308
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 18:54:08 +0300

    Added Task5 from Methods

---
+++ Lesson3/Methods/Methods_Task5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods_Task5
+{
+    class Program
+    {
+        static int z(int x, int y)
+        {
+            var ret = 2 * f(x) + 4 * f(y);
+
+            return ret;
+        }
+
+        static int f(int x)
+        {
+            if (x >= 0)
+            {
+                return 5 * x;
+            }
+
+            if (x < 0)
+            {
+                return -2 * x;
+            }
+
+            return 0;
+        }
+
+        static void Main()
+        {
+            var x = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+
+            var ans = z(x, y);
+
+            Console.WriteLine(ans);
+        }
+    }
+}
