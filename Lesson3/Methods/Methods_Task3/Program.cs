039793d7d35eed5f1937d4c5644d004ceed7fb5b
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 15:15:54 +0300

    Added Task3 and fixed Task2 from Methods

--- Lesson3/Methods/Methods_Task3/Program.cs
+++ Lesson3/Methods/Methods_Task3/Program.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 
-namespace Methods_Task2
+namespace Methods_Task3
 {
     class Program
     {
-        static double Average(double[] a)
+        static double AVGeometric(double[] a)
         {
-            var sum = 0.0;
-
+            var p = 1.0;
             for (int i = 0; i < 10; ++i)
             {
-                sum += a[i];
+                p *= a[i];
             }
 
-            return sum / 10;
+            var geom = Math.Pow(p, 1.0 / 10.0);
+
+            return geom;
         }
+
         static void Main()
         {
             var a = new double[10];
+
             for (int i = 0; i < 10; ++i)
             {
                 a[i] = double.Parse(Console.ReadLine());
             }
 
-            var ans = Average(a);
+            var ans = AVGeometric(a);
 
             Console.WriteLine(ans);
         }
