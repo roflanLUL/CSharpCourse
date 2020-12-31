0ca9b414e738ff1dfade5c428d003a0091521102
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 14:50:12 +0300

    Added Task2 from Methods

---
+++ Lesson3/Methods/Methods_Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Methods_Task2
+{
+    class Program
+    {
+        static double Average(int[] a)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                sum += a[i];
+            }
+
+            return sum / 10;
+        }
+        static void Main()
+        {
+            var a = new int[10];
+            for (int i = 0; i < 10; ++i)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            var ans = Average(a);
+
+            Console.WriteLine(ans);
+        }
+    }
+}
