fb37e44040f35e1e6818f6efb613e51724994c69
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 20:53:30 +0300

    Merged two subtasks from Task6 from Methods into one

---
+++ Lesson3/Methods/Methods_Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Methods_Task6
+{
+    class Program
+    {
+        static void Inc(ref int k)
+        {
+            k++;
+        }
+
+        static void Dec(ref int k)
+        {
+            k--;
+            if (k == 0)
+            {
+                k = 10;
+            }
+        }
+        static void Main()
+        {
+            var cnt_inc = 0;
+            var cnt_dec = 10;
+            Inc(ref cnt_inc);
+            Dec(ref cnt_dec);
+            Console.WriteLine(cnt_inc);
+            Console.WriteLine(cnt_dec);
+        }
+    }
+}
