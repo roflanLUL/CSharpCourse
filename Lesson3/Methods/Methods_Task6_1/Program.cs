d4c7d33e1a7ee5b00d8b02f9457b62e8d8deae21
Sergey Gordeev <duamaster1504@yandex.ru>
2020-12-31 19:11:43 +0300

    Added Task6 from Methods

---
+++ Lesson3/Methods/Methods_Task6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods_Task6_1
+{
+    class Program
+    {
+        static void Inc(ref int k)
+        {
+            k++;
+        }
+
+        static void Main()
+        {
+            var k = 0;
+            Inc(ref k);
+            Console.WriteLine(k);
+        }
+    }
+}
---
+++ Lesson3/Methods/Methods_Task6_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Methods_Task6_2
+{
+    class Program
+    {
+        static void Dec(ref int k)
+        {
+            k--;
+            if (k == 0)
+            {
+                k = 10;
+            }
+        }
+
+        static void Main()
+        {
+            var k = 10;
+            Dec(ref k);
+            Console.WriteLine(k);
+        }
+    }
+}
