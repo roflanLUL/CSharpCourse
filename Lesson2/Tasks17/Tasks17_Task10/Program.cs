58103ff350fb0368d0157473fbc22977744a1a18
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 17:59:32 +0300

    Added Task10 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task10/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Tasks17_Task10
+{
+    class Program
+    {
+        static bool isPrime(int x)
+        {
+            bool isPrime = true;
+            for (int i = 2; i <= Math.Sqrt(x); ++i)
+            {
+                if (x % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 3577;
+            for (int i = 3578; i <= 8009; ++i)
+            {
+                if (isPrime(i) && (i % 10 == 7))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
