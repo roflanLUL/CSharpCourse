a738f842a1d581a8248f45ecc98bf74b8cfff082
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 16:36:24 +0300

    Added Task9 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task9
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var minim = 8010;
+            for (int i = 3578; i <= 8009; ++i)
+            {
+                if ((i % 17 == 0) && (i % 93 != 0) && (i % 100 == 11))
+                {
+                    if (i < minim)
+                    {
+                        minim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {minim}");
+        }
+    }
+}
