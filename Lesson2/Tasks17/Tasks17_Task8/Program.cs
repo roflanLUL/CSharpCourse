7c737edf91ce5f36e1ec13021f48392713e79fed
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 16:20:36 +0300

    Added Task8 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task8
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 1567;
+            for (int i = 1568; i <= 9956; ++i)
+            {
+                if (((int)Math.Sqrt(i) * (int)Math.Sqrt(i) == i) && (i % 9 != 0) && (i % 25 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
