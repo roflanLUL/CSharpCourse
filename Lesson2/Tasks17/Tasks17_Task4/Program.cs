1362b8d432efd04a42faedc79ac619ff8d5e609d
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 15:44:01 +0300

    Added Task4 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 9999;
+            for (int i = 10000; i <= 100000; ++i)
+            {
+                if ((i % 23 == 0) && (i % 5 != 0) && (i % 6 != 0) && (i % 7 != 0) && (i % 8 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
