f14a0af91833dc34dac25006b82ce19aa70c72b6
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 16:04:08 +0300

    Added Task7 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tasks17_Task7
+{
+    class Program
+    {
+        static void Main()
+        {
+            var maxim = 2764;
+            var predmax = 2763;
+            for (int i = 2765; i <= 9525; ++i)
+            {
+                if ((i % 205 == 0) && (i % 10 != 0) && (i % 16 != 0) && (i % 82 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        predmax = maxim;
+                        maxim = i;
+                    }
+                    else if (i > predmax)
+                    {
+                        predmax = i;
+                    }
+                }
+            }
+            Console.WriteLine($"{predmax} {maxim}");
+        }
+    }
+}
