97187039d116d51b1eadb5435a31e52cbde90c6f
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 15:33:28 +0300

    Added Task1 and Task2 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 2076;
+            for (int i = 2077; i <= 8276; ++i)
+            {
+                if ((i % 6 == 0) && (i % 4 != 0) && (i % 9 != 0) && (i % 20 != 0) && (i % 36 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
---
+++ Lesson2/Tasks17/Tasks17_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 1577;
+            for (int i = 1578; i <= 6237; ++i)
+            {
+                if ((i % 4 == 0) && (i % 3 != 0) && (i % 16 != 0) && (i % 12 != 0) && (i % 8 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
