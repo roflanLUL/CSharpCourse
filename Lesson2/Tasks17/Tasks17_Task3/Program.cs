8353569372203b3d74656fbb29a92554f6e46341
Sergey Gordeev <duamaster1504@yandex.ru>
2021-01-02 15:37:59 +0300

    Added Task3 from Tasks17

---
+++ Lesson2/Tasks17/Tasks17_Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tasks17_Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cnt = 0;
+            var maxim = 9012;
+            for (int i = 9013; i <= 23800; ++i)
+            {
+                if ((i % 9 == 0) && (i % 2 != 0) && (i % 6 != 0) && (i % 12 != 0) && (i % 81 != 0))
+                {
+                    if (i > maxim)
+                    {
+                        maxim = i;
+                    }
+                    cnt++;
+                }
+            }
+            Console.WriteLine($"{cnt} {maxim}");
+        }
+    }
+}
